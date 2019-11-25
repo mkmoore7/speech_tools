f920b911c7639e2f530a7f66845cf4a94a27b40e
Meredith Moore (Student) <mkmoore7@asu.edu>
2019-11-25 09:22:44 -0700

    fixed bugs in extract_features

--- extract_features.py
+++ extract_features.py
@@ -8,19 +8,26 @@ import argparse
 import os
 import pickle as pkl
 
-def make_melspectrograms(data_loc, save_loc, nfft, hop, win_len, fmax):
+def make_melspectrograms(data_loc, save_loc):
 
     data = []
     #iterate through the given data directory
     for filename in os.listdir(data_loc):
-
+        print(filename)
+        if filename.split('.')[-1] not in ['wav', 'mp3']:
+            break
         #open the speech, and make the spectrogram
         y, sr = librosa.load(os.path.join(data_loc, filename))
-        spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=nfft, hop_length=hop, win_length=win_len, fmax=fmax)
+
+        #TODO: attach arguments, right now it's hard coded the Google way
+        y = librosa.core.resample(y, sr, 16000)
+        spect = librosa.feature.melspectrogram(y=y, sr=16000, n_fft=1024, hop_length=200, fmax=7600, n_mels=80)
+        #convert to log-scale (dbs)
+        ps_db = librosa.power_to_db(spect, ref=np.max)
 
         #save the spectrogram... add to a list and save the list at the end.
-        data.append([filename, spect])
-        pkl.dump(open(os.path.join(save_loc, 'melspect.pkl'), 'wb'))
+        data.append([filename, ps_db])
+    pkl.dump(data, open(os.path.join(save_loc, 'melspect.pkl'), 'wb'))
 
 
 
@@ -28,13 +35,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='File to extract features from audio')
     parser.add_argument('--feature')
     parser.add_argument('--data_dir')
-    parser.add_argument('--save_loc')
-    parser.add_argument('--nfft')
-    parser.add_argument('--hop')
-    parser.add_argument('--win_len')
-    parser.add_argument('--fmax')
-    args = parser.parser_args()
+    parser.add_argument('--save_dir')
+    args = parser.parse_args()
 
     if args.feature == 'melspect':
-        make_melspectrograms(args.data_dir, args.save_loc, args.nfft, args.hop, args.win_len, args.fmax)
+        make_melspectrograms(args.data_dir, args.save_dir)
+
 
